c348237f0d585de0adfa43a5e1fb5d1244a9335f
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-25 14:45:47 +0900

    set routings of user, article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "articles#index"
+
+  resources :user, only: [:edit, :update]
+  resources :article, only: [:index, :new, :create, :edit, :show, :update]
 end
