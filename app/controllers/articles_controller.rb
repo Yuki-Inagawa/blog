4f914f1c91939f3b324d65fc5b7a24e89950296b
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-26 09:55:55 +0900

    update articles controller
    defined new, create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    Article.create(image: aricle_params[:image],content: article_params[:content],user: current_user.id)
+    @article = Article.new
   end
 
   def show
   end
 
   def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:content, :image).merge(user: current_user.id)
   end
 end
