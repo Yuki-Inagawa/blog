2a98013378253d5c92be89ad52ed44f09dacaa68
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-26 13:24:01 +0900

    create user_id column in article model

--- db/migrate/20190625084419_adduser_toarticles.rb
+++
@@ -1,6 +0,0 @@
-class AdduserToarticles < ActiveRecord::Migration[5.0]
-  def change
-    # add_column :articles, :user, :references [null: false, foreign_key: true]
-    # add_column :articles, :user, :string
-  end
-end
