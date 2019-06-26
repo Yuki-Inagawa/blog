7220f3f3468161444ce3f2d28f258d4ad261a7c6
Yuki-Inagawa <yuki2012mu@gmai.com>
2019-06-26 13:22:12 +0900

    update articles controller
    to define index action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
+    @articles = Article.all
+    # .page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
@@ -12,10 +15,11 @@ class ArticlesController < ApplicationController
 
   def create
     Article.create(article_params)
+    redirect_to root_path
   end
 
   private
   def article_params
-    params.require(:article).permit(:content, :image).merge(user: current_user.id)
+    params.require(:article).permit(:content).merge(user_id: current_user.id)
   end
 end
